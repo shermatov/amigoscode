c20f3fc71e8a13da345dc20bc8b6dcc5d7ab8736
Aibek Shermatov <75265467+shermatov@users.noreply.github.com>
2025-07-07 01:24:22 +0200

    Update CustomerService.java
    Signed-off-by: shermatov <aibekshermatovv@gmail.com>

--- src/main/java/com/shermatov/customer/Customer.java
+++ src/main/java/com/shermatov/customer/Customer.java
@@ -48,4 +48,5 @@ public class Customer {
 
     @Column(nullable = false)
     private Integer age;
+
 }
---
+++ src/main/java/com/shermatov/customer/CustomerJDBCDAOService.java
@@ -0,0 +1,50 @@
+package com.shermatov.customer;
+
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerJDBCDAOService implements CustomerDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CustomerJDBCDAOService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void deleteCustomerById(Integer customerID) {
+
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer customerID) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public boolean existsCustomerWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existsCustomerWithID(Integer customerId) {
+        return false;
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
+}
