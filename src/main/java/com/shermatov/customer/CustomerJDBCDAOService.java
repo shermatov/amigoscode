093fb1c774c04fe41af7b06618f2fa6cbdb12706
shermatov <aibekshermatovv@gmail.com>
2025-07-09 14:19:26 +0200

    I would like to see, from what username is sending data.
    Signed-off-by: shermatov <aibekshermatovv@gmail.com>

--- src/main/java/com/shermatov/customer/CustomerJDBCDAOService.java
+++ src/main/java/com/shermatov/customer/CustomerJDBCDAOService.java
@@ -1,50 +1,107 @@
 package com.shermatov.customer;
 
-import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository("jdbc")
 public class CustomerJDBCDAOService implements CustomerDAO {
 
     private final JdbcTemplate jdbcTemplate;
+    private final CustomerRowMapper customerRowMapper;
 
-    public CustomerJDBCDAOService(JdbcTemplate jdbcTemplate) {
+
+    public CustomerJDBCDAOService(JdbcTemplate jdbcTemplate, CustomerRowMapper customerRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.customerRowMapper = customerRowMapper;
     }
 
     @Override
-    public void deleteCustomerById(Integer customerID) {
-
+    public void deleteCustomerById(Integer id) {
+        var sql = "DELETE FROM customer WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 
     @Override
     public List<Customer> selectAllCustomers() {
-        return List.of();
+        var sql = """
+                SELECT * from customer
+                """;
+
+        return jdbcTemplate.query(sql,(customerRowMapper));
+
     }
 
     @Override
-    public Optional<Customer> selectCustomerById(Integer customerID) {
-        return Optional.empty();
+    public Optional<Customer> selectCustomerById(Integer id) {
+        var sql = """
+                SELECT * from customer WHERE id = ?
+                """;
+
+        return jdbcTemplate.query(sql, customerRowMapper, id)
+                .stream().findFirst();
     }
 
     @Override
     public void insertCustomer(Customer customer) {
-        var sql = "INSERT INTO customer (first_name, last_name) VALUES (?, ?)";
+        var sql = """
+    INSERT INTO customer (id, name, email, age)
+    VALUES (?, ?, ?, ?)
+    """;
+        jdbcTemplate.update(
+                sql,
+                customer.getId(),
+                customer.getName(),
+                customer.getEmail(),
+                customer.getAge()
+        );
+
     }
 
     @Override
     public boolean existsCustomerWithEmail(String email) {
-        return false;
+        var sql = """
+                SELECT count(email)
+                FROM customer
+                WHERE email = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
+
+        return  count != null && count > 0;
     }
 
     @Override
     public boolean existsCustomerWithID(Integer customerId) {
-        return false;
+        var sql = """
+                SELECT count(id)
+                FROM customer
+                WHERE id = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, customerId);
+
+        return  count != null && count > 0;
     }
 
     @Override
     public void updateCustomer(Customer update) {
+        if(update.getName() != null) {
+            String sql = "UPDATE customer SET name = ? WHERE id = ?";
+            int result = jdbcTemplate.update(sql, update.getName(), update.getId());
+            System.out.println("jdbcTemplate.update: " + result);
+        }
 
+        if(update.getEmail() != null) {
+            String sql = "UPDATE customer SET email = ? WHERE id = ?";
+            int result = jdbcTemplate.update(sql, update.getEmail(), update.getId());
+            System.out.println("jdbcTemplate.update: " + result);
+        }
+        if(update.getAge() != null) {
+            String sql = "UPDATE customer SET age = ? WHERE id = ?";
+            int result = jdbcTemplate.update(sql, update.getAge(), update.getId());
+            System.out.println("jdbcTemplate.update: " + result);
+        }
     }
 }
